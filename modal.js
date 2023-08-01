c1b91de1fe8da939d5e2872f7484c97962315fa6
marcelovaldemarsilva <119859720+marcelovaldemarsilva@users.noreply.github.com>
2023-08-01 12:15:49 -0300

    Add files via upload

---
+++ modal.js
@@ -0,0 +1,8 @@
+function Modal(){
+
+const background = document.querySelector('#background-modal')
+const modalCorpo = document.querySelector('#modal-corpo')
+
+background.classList.toggle('background-modal')
+modalCorpo.classList.toggle('modal-corpo')
+}
